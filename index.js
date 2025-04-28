a2795a810ab8925bd256602a6e66ab64b0c6b5c2
emanuelmisic <emanuel64@gmail.com>
2025-04-28 20:53:42 +0200

    improve cards look & add js

---
+++ index.js
@@ -0,0 +1,180 @@
+const nav = document.querySelector("#navbar");
+const dashboard = document.querySelector("#dashboard");
+
+const CONFIG = {
+  svgWidth: 22,
+  svgHeight: 22,
+};
+
+const navElements = [
+  {
+    title: "Home",
+    icon: "house-door-fill",
+    svgPath:
+      '<path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5"/>',
+  },
+  {
+    title: "Profile",
+    icon: "person-vcard-fill",
+    svgPath:
+      '<path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm9 1.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 0-1h-4a.5.5 0 0 0-.5.5M9 8a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 0-1h-4A.5.5 0 0 0 9 8m1 2.5a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 0-1h-3a.5.5 0 0 0-.5.5m-1 2C9 10.567 7.21 9 5 9c-2.086 0-3.8 1.398-3.984 3.181A1 1 0 0 0 2 13h6.96q.04-.245.04-.5M7 6a2 2 0 1 0-4 0 2 2 0 0 0 4 0"/>',
+  },
+  {
+    title: "Messages",
+    icon: "chat-left-fill",
+    svgPath:
+      '<path d="M2 0a2 2 0 0 0-2 2v12.793a.5.5 0 0 0 .854.353l2.853-2.853A1 1 0 0 1 4.414 12H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>',
+  },
+  {
+    title: "History",
+    icon: "clock-fill",
+    svgPath:
+      '<path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71z"/>',
+  },
+  {
+    title: "Tasks",
+    icon: "stickies-fill",
+    svgPath: `
+        <path d="M0 1.5V13a1 1 0 0 0 1 1V1.5a.5.5 0 0 1 .5-.5H14a1 1 0 0 0-1-1H1.5A1.5 1.5 0 0 0 0 1.5"/>
+        <path d="M3.5 2A1.5 1.5 0 0 0 2 3.5v11A1.5 1.5 0 0 0 3.5 16h6.086a1.5 1.5 0 0 0 1.06-.44l4.915-4.914A1.5 1.5 0 0 0 16 9.586V3.5A1.5 1.5 0 0 0 14.5 2zm6 8.5a1 1 0 0 1 1-1h4.396a.25.25 0 0 1 .177.427l-5.146 5.146a.25.25 0 0 1-.427-.177z"/>
+    `,
+  },
+  {
+    title: "Communities",
+    icon: "people-fill",
+    svgPath:
+      '<path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5.784 6A2.24 2.24 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.3 6.3 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1zM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5"/>',
+  },
+  {
+    title: "Settings",
+    icon: "gear-fill",
+    svgPath:
+      '<path d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z"/>',
+  },
+  {
+    title: "Support",
+    icon: "question-square-fill",
+    svgPath:
+      '<path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm3.496 6.033a.237.237 0 0 1-.24-.247C5.35 4.091 6.737 3.5 8.005 3.5c1.396 0 2.672.73 2.672 2.24 0 1.08-.635 1.594-1.244 2.057-.737.559-1.01.768-1.01 1.486v.105a.25.25 0 0 1-.25.25h-.81a.25.25 0 0 1-.25-.246l-.004-.217c-.038-.927.495-1.498 1.168-1.987.59-.444.965-.736.965-1.371 0-.825-.628-1.168-1.314-1.168-.803 0-1.253.478-1.342 1.134-.018.137-.128.25-.266.25h-.825zm2.325 6.443c-.584 0-1.009-.394-1.009-.927 0-.552.425-.94 1.01-.94.609 0 1.028.388 1.028.94 0 .533-.42.927-1.029.927"/>',
+  },
+  {
+    title: "Privacy",
+    icon: "shield-fill-check",
+    svgPath:
+      '<path fill-rule="evenodd" d="M8 0c-.69 0-1.843.265-2.928.56-1.11.3-2.229.655-2.887.87a1.54 1.54 0 0 0-1.044 1.262c-.596 4.477.787 7.795 2.465 9.99a11.8 11.8 0 0 0 2.517 2.453c.386.273.744.482 1.048.625.28.132.581.24.829.24s.548-.108.829-.24a7 7 0 0 0 1.048-.625 11.8 11.8 0 0 0 2.517-2.453c1.678-2.195 3.061-5.513 2.465-9.99a1.54 1.54 0 0 0-1.044-1.263 63 63 0 0 0-2.887-.87C9.843.266 8.69 0 8 0m2.146 5.146a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 7.793z"/>',
+  },
+];
+
+const dashboardElements = [
+  {
+    title: "Super cool project",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat qui doloremque enim non praesentium a!",
+    actions: {
+      projectLink: "project1",
+      githubLink: "repo1",
+    },
+  },
+  {
+    title: "Super cool project",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat qui doloremque enim non praesentium a!",
+    actions: {
+      projectLink: "project2",
+      githubLink: "repo2",
+    },
+  },
+  {
+    title: "Super cool project",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat qui doloremque enim non praesentium a!",
+    actions: {
+      projectLink: "project3",
+      githubLink: "repo3",
+    },
+  },
+  {
+    title: "Super cool project",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat qui doloremque enim non praesentium a!",
+    actions: {
+      projectLink: "project4",
+      githubLink: "repo4",
+    },
+  },
+];
+
+renderNavElements();
+renderDashboardElements();
+
+function renderNavElements() {
+  const ul = document.createElement("ul");
+  nav.appendChild(ul);
+
+  for (const item of navElements) {
+    const li = document.createElement("li");
+    li.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" width="${CONFIG.svgWidth}" height="${CONFIG.svgHeight}" fill="currentColor" class="bi bi-${item.icon}" viewBox="0 0 18 18">
+            ${item.svgPath}
+        </svg>
+    `;
+    li.insertAdjacentHTML("beforeend", item.title);
+    ul.appendChild(li);
+  }
+}
+
+function renderDashboardElements() {
+  for (const item of dashboardElements) {
+    const card = document.createElement("div");
+    card.className = "card";
+
+    const title = document.createElement("span");
+    title.innerHTML = item.title;
+    card.appendChild(title);
+
+    const description = document.createElement("p");
+    description.innerHTML = item.description;
+    card.appendChild(description);
+
+    const actions = document.createElement("div");
+    actions.className = "actions";
+    actions.innerHTML = `
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        fill="currentColor"
+        viewbox="0 0 16 16"
+        class="bi bi-eye"
+        onclick="handleClick('${item.actions.projectLink}')"
+    >
+        <path
+        d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z"
+        />
+        <path
+        d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0"
+        />
+    </svg>
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        fill="currentColor"
+        viewbox="0 0 16 16"
+        class="bi bi-github"
+        onclick="handleClick('${item.actions.githubLink}')"
+    >
+        <path
+        d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"
+        ></path>
+    </svg>
+    `;
+
+    card.appendChild(actions);
+    dashboard.appendChild(card);
+  }
+}
+
+function handleClick(link) {
+  console.log(link);
+}
